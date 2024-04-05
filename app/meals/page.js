919556930bffc2a6bcb169271a1f021e4f5849c1
Sameer Sondhi <sameer.sondhi@outlook.com>
2024-04-04 19:48:11 -0700

    Adding complete code

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,23 @@
 import Link from "next/link";
 import classes from './page.module.css'
+import MealsGrid from "@/components/meals/meals-grid";
+import {getMeals} from '@/lib/meals'
+import { Suspense } from "react";
+
+
+export const metadata = {
+    title: 'All Meals',
+    description: 'Browse the delicious meals shared by our vibrant community',
+  };
+
+async function Meals(){
+    const meals = await getMeals();
+    return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage(){
+    
+
     return (
     <>
     <header className={classes.header}>
@@ -12,14 +28,14 @@ export default function MealsPage(){
         <p>
             Choose your favorite recipe and cook it yourself. It is easy and fun.
         </p>
-        <p classname={classes.link}>
-            <Link href="/meals/share">Share your favorite recipe</Link>
+        <p className={classes.cta}>
+            <Link href="/meals/share">Share Your Favorite Recipe</Link>
         </p>
     </header>
-    <main>
-        <h1>Selection of Meals</h1>
-        <p><Link href="/meals/meal-1">First Meal</Link></p>
-        <p><Link href="/meals/meal-2">Second Meal</Link></p>
+    <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+        <Meals />
+        </Suspense>
     </main>
     </>
     )
